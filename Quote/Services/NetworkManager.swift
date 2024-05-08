8efbe6e818c3cfc7efc7138c12df7bf2997a1426
Amirov Foma <amirov.foma@icloud.com>
2024-05-08 20:13:10 +0300

    add: Added image on mainVC

--- Quote/Services/NetworkManager.swift
+++ Quote/Services/NetworkManager.swift
@@ -7,6 +7,12 @@
 
 import Foundation
 
+enum NetworkError: Error {
+    case invalidURL
+    case noData
+    case decodingError
+}
+
 enum List: String {
     case url = "https://favqs.com/api/qotd"
 }
@@ -14,6 +20,23 @@ enum List: String {
 final class NetworkManager {
     static let shared = NetworkManager()
     
+    func fetchQuotes(url: String, completion: @escaping(Quote) -> Void) {
+        guard let url = URL(string: url) else { return }
+        
+        URLSession.shared.dataTask(with: url) { data, _, error in
+            guard let data else {
+                return
+            }
+            
+            do {
+                let quoteApp = try JSONDecoder().decode(QuoteApp.self, from: data)
+                completion(quoteApp.quote)
+            } catch {
+                print(error)
+            }
+        }.resume()
+    }
     
+    private init() {}
 }
 
--- Quote/ViewControllers/ViewController.swift
+++ Quote/ViewControllers/ViewController.swift
@@ -8,14 +8,24 @@
 import UIKit
 
 final class ViewController: UIViewController {
-   
+    
+//    var quote =
+    
+    @IBOutlet var quoteImage: UIImageView!
     @IBOutlet var bodyLabel: UILabel!
     @IBOutlet var authorLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        quoteImage.image = UIImage(named: "quotes")
+//        fetchQuote()
     }
-
     
+//    private func fetchQuote() {
+//        NetworkManager.shared.fetchQuotes(url: List.url.rawValue) { [weak self] quote in
+//            self.quote = quote
+//        }
+//    }
+
 }
 
